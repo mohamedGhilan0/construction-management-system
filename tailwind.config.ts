5e32bcd840966f76e0bfc03c190876854776aaa9
mohamedGhilan0 <mohamedghilan0@gmail.com>
2026-09-22 11:36:11 +0300

    chore: bootstrap tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,2 @@
+import type { Config } from "tailwindcss";
+export default {darkMode:"class",content:["./src/**/*.{js,ts,jsx,tsx,mdx}"],theme:{extend:{colors:{brand:{900:"#15987f",500:"#08d1aa",100:"#fff5ad",50:"#fffced"}}}},plugins:[]} satisfies Config;
